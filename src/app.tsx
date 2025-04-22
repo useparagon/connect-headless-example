2b070472d584e5cbbc453c5e261e68fe610995d6
Richard Bidin <richard.bidin@useparagon.com>
2025-04-22 21:27:24 +0200

    chore: setup repo with Vite + Tw + Shadcn

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+
+export function App() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h1>
+        Hello <span className="line-through text-black/50">World</span> Paragon
+      </h1>
+      <Button onClick={() => setCounter((c) => c + 1)}>Count: {counter}</Button>
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App } from './app';
+
+function main() {
+  const rootElement = document.getElementById('root');
+
+  if (!rootElement) {
+    throw new Error('Root element not found');
+  }
+
+  const root = createRoot(rootElement);
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+main();
