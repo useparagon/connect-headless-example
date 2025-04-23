f22552561357d88459319563569effba93775423
Richard Bidin <richard.bidin@useparagon.com>
2025-04-23 20:25:00 +0200

    chore: authentication setup with the SDK

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,21 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from '@/app';
 
-function main() {
-  window.paragon = paragon;
+const projectId = import.meta.env.VITE_PARAGON_PROJECT_ID;
+const jwtToken = import.meta.env.VITE_PARAGON_JWT_TOKEN;
 
+async function main() {
   const rootElement = document.getElementById('root');
 
   if (!rootElement) {
     throw new Error('Root element not found');
+  }
+
+  try {
+    await paragon.authenticate(projectId, jwtToken);
+    console.log('Successfully authenticated!');
+  } catch (error) {
+    throw new Error(`Failed to authenticate with Paragon: ${error}`);
   }
 
   const root = createRoot(rootElement);
