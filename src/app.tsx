1a87d39afb777c8ee065f5be52971fae01949325
Richard Bidin <richard.bidin@useparagon.com>
2025-04-23 22:47:14 +0200

    chore: initial UI

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,77 @@
-import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { paragon } from '@useparagon/connect';
 
 import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardTitle } from '@/components/ui/card';
 
 export function App() {
-  const [counter, setCounter] = useState(0);
+  const user = paragon.getUser();
+  const { data: integrations, isLoading } = useIntegrationMetadata();
+
+  if (isLoading || !integrations) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      <h1>
-        Hello <span className="line-through text-black/50">World</span> Paragon
-      </h1>
-      <Button onClick={() => setCounter((c) => c + 1)}>Count: {counter}</Button>
+      <header className="flex items-center py-4 px-8 border-b border-border">
+        <img
+          src="https://dashboard.useparagon.com/images/icons/paragon-no-text.svg"
+          className="w-7 h-7 mr-2"
+        />
+        <div
+          className="text-sm font-bold mr-8"
+          style={{ lineHeight: '1.1rem' }}
+        >
+          Paragon{' '}
+          <p
+            className="text-xs opacity-70 uppercase font-extrabold"
+            style={{ fontSize: 10 }}
+          >
+            Headless Connect
+          </p>
+        </div>
+      </header>
+
+      <div className="py-4 px-8">
+        <h1 className="text-2xl font-medium mb-4">Integrations</h1>
+        <ul className="flex flex-wrap gap-4">
+          {integrations.map((integration) => {
+            const integrationEnabled =
+              user.authenticated &&
+              user.integrations[integration.type]?.enabled;
+
+            return (
+              <li key={integration.type}>
+                <Card className="min-w-[300px] hover:shadow-sm transition-shadow">
+                  <CardContent>
+                    <CardTitle>
+                      <div className="flex gap-2 items-center justify-between">
+                        <div className="flex gap-2 items-center">
+                          <img src={integration.icon} width={30} />
+                          {integration.name}
+                        </div>
+                        <Button onClick={() => {}} className="cursor-pointer">
+                          {integrationEnabled ? 'Manage' : 'Enable'}
+                        </Button>
+                      </div>
+                    </CardTitle>
+                  </CardContent>
+                </Card>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
+}
+
+function useIntegrationMetadata() {
+  return useQuery({
+    queryKey: ['integrations'],
+    queryFn: () => {
+      return paragon.getIntegrationMetadata();
+    },
+  });
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { paragon } from '@useparagon/connect';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { App } from '@/app';
+
+const queryClient = new QueryClient();
 
 const projectId = import.meta.env.VITE_PARAGON_PROJECT_ID;
 const jwtToken = import.meta.env.VITE_PARAGON_JWT_TOKEN;
 
 async function main() {
+  window.paragon = paragon;
+
   const rootElement = document.getElementById('root');
 
   if (!rootElement) {
@@ -25,7 +30,9 @@ async function main() {
 
   root.render(
     <StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </StrictMode>
   );
 }
