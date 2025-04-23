159d851ab3a7bf8edb77dbbd27af92b0618f7974
Richard Bidin <richard.bidin@useparagon.com>
2025-04-23 23:44:50 +0200

    chore: initial setup modal for an integration

--- src/app.tsx
+++ src/app.tsx
@@ -5,40 +5,46 @@ import { Header } from '@/components/layout/header';
 import { IntegrationCard } from '@/components/feature/integration-card';
 
 export function App() {
-  const user = paragon.getUser();
-  const { data: integrations, isLoading } = useIntegrationMetadata();
-
-  if (isLoading || !integrations) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div>
       <Header />
 
       <div className="py-4 px-8">
         <h1 className="text-2xl font-medium mb-4">Integrations</h1>
-        <ul className="flex flex-wrap gap-4">
-          {integrations.map((integration) => {
-            const integrationEnabled =
-              user.authenticated &&
-              user.integrations[integration.type]?.enabled;
-
-            return (
-              <li key={integration.type}>
-                <IntegrationCard
-                  key={integration.type}
-                  type={integration.type}
-                  name={integration.name}
-                  icon={integration.icon}
-                  enabled={integrationEnabled ?? false}
-                />
-              </li>
-            );
-          })}
-        </ul>
+        <IntegrationList />
       </div>
     </div>
+  );
+}
+
+function IntegrationList() {
+  const user = paragon.getUser();
+  const { data: integrations, isLoading } = useIntegrationMetadata();
+  if (isLoading || !integrations) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <ul className="flex flex-wrap gap-4">
+        {integrations.map((integration) => {
+          const integrationEnabled =
+            user.authenticated && user.integrations[integration.type]?.enabled;
+
+          return (
+            <li key={integration.type}>
+              <IntegrationCard
+                key={integration.type}
+                type={integration.type}
+                name={integration.name}
+                icon={integration.icon}
+                enabled={integrationEnabled ?? false}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
--- src/components/feature/integration-card.tsx
+++ src/components/feature/integration-card.tsx
@@ -1,5 +1,16 @@
+import { useState } from 'react';
+
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { useQuery } from '@tanstack/react-query';
+import { paragon } from '@useparagon/connect';
 
 type Props = {
   type: string;
@@ -9,6 +20,8 @@ type Props = {
 };
 
 export function IntegrationCard(props: Props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <Card className="min-w-[300px] hover:shadow-sm transition-shadow">
       <CardContent>
@@ -18,12 +31,64 @@ export function IntegrationCard(props: Props) {
               <img src={props.icon} width={30} />
               {props.name}
             </div>
-            <Button onClick={() => {}} className="cursor-pointer">
+            <Button
+              className="cursor-pointer"
+              onClick={() => setIsModalOpen(true)}
+            >
               {props.enabled ? 'Manage' : 'Enable'}
             </Button>
+            {isModalOpen && (
+              <IntegrationModal onOpenChange={setIsModalOpen} {...props} />
+            )}
           </div>
         </CardTitle>
       </CardContent>
     </Card>
   );
+}
+
+function IntegrationModal(
+  props: Props & {
+    onOpenChange: (open: boolean) => void;
+  }
+) {
+  const { data: integrationConfig, isLoading } = useIntegrationConfig(
+    props.type
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!integrationConfig) {
+    throw new Error(`Integration config not found for ${props.type}`);
+  }
+
+  return (
+    <Dialog onOpenChange={props.onOpenChange} open>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <div className="flex gap-4 items-center">
+            <img src={props.icon} width={45} />
+            <div className="flex flex-col gap-1">
+              <DialogTitle>{props.name}</DialogTitle>
+              <DialogDescription>
+                {integrationConfig.shortDescription}
+              </DialogDescription>
+            </div>
+          </div>
+        </DialogHeader>
+        <div className="py-4">Content for {props.name}...</div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+function useIntegrationConfig(type: string) {
+  return useQuery({
+    queryKey: ['integrationConfig', type],
+    queryFn: () => {
+      return paragon.getIntegrationConfig(type);
+    },
+  });
 }
