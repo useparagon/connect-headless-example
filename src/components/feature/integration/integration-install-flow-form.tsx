c8b28c0e9110aac27f8b368124215fa5bcac050a
Richard Bidin <richard.bidin@useparagon.com>
2025-07-10 12:56:18 -0300

    feat: moving to react-hook-form and supports defaultValue

--- src/components/feature/integration/integration-install-flow-form.tsx
+++ src/components/feature/integration/integration-install-flow-form.tsx
@@ -1,5 +1,11 @@
-import { useState } from 'react';
-import { ConnectInputValue, paragon, SerializedConnectInput } from '@useparagon/connect';
+import {
+  AccountType,
+  ConnectInputValue,
+  IntegrationConnectInput,
+  paragon,
+  SerializedConnectInput,
+} from '@useparagon/connect';
+import { Controller, useForm } from 'react-hook-form';
 
 import { Button } from '@/components/ui/button';
 import { SerializedConnectInputPicker } from '@/components/feature/serialized-connect-input-picker';
@@ -13,102 +19,143 @@ type Props = {
   onFinishPreOptions: (preOptions: Record<string, ConnectInputValue>) => void;
   onFinishPostOptions: (postOptions: Record<string, ConnectInputValue>) => void;
   error: Error | null;
+};
+
+export function IntegrationInstallFlowForm(props: Props) {
+  switch (props.installFlowStage.stage) {
+    case 'accountType':
+      return (
+        <AccountTypePicker
+          options={props.installFlowStage.options}
+          onSelect={props.onSelectAccount}
+        />
+      );
+    case 'preOptions':
+      return (
+        <PreOptionsForm
+          integration={props.integration}
+          options={props.installFlowStage.options}
+          onSubmit={props.onFinishPreOptions}
+          error={props.error}
+        />
+      );
+    case 'postOptions':
+      return (
+        <PostOptionsForm
+          integration={props.integration}
+          options={props.installFlowStage.options}
+          onSubmit={props.onFinishPostOptions}
+          error={props.error}
+        />
+      );
+    case 'done':
+    default:
+      return null;
+  }
 }
 
-export function FlowForm(props: Props) {
-  const [preOptions, setPreOptions] = useState<
-    Record<string, ConnectInputValue>
-  >({});
-  const [postOptions, setPostOptions] = useState<
-    Record<string, ConnectInputValue>
-  >({});
-
-  if (props.installFlowStage.stage === 'accountType') {
-    return (
-      <div>
-        <div>
-          <h2>Select an account</h2>
-          <div className="flex flex-col gap-2 items-start">
-            {props.installFlowStage.options.map((option) => (
-              <Button
-                key={option.id}
-                type="button"
-                onClick={() => {
-                  props.onSelectAccount(option.id);
-                }}
-              >
-                {option.accountDescription}
-              </Button>
-            ))}
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  if (props.installFlowStage.stage === 'preOptions') {
-    return (
-      <div className="flex flex-col gap-4">
-        {props.installFlowStage.options.map((option) => (
-          <SerializedConnectInputPicker
+function AccountTypePicker(props: {
+  options: AccountType[];
+  onSelect: (accountId: string) => void;
+}) {
+  return (
+    <div>
+      <h2>Select an account</h2>
+      <div className="flex flex-col gap-2 items-start">
+        {props.options.map((option) => (
+          <Button
             key={option.id}
-            integration={props.integration}
-            field={option as SerializedConnectInput}
-            value={preOptions[option.id]}
-            onChange={(value) => {
-              setPreOptions((current) => ({
-                ...current,
-                [option.id]: value,
-              }));
+            type="button"
+            onClick={() => {
+              props.onSelect(option.id);
             }}
-          />
+          >
+            {option.accountDescription}
+          </Button>
         ))}
-        <Button
-          onClick={() => {
-            props.onFinishPreOptions(preOptions);
-          }}
-        >
-          Next
-        </Button>
-        {props.error ? (
-          <div className="flex flex-col gap-2">
-            <p className="font-medium">Something went wrong</p>
-            <pre className="text-red-500 max-w-full text-sm bg-red-50 p-2 rounded-md border border-red-200">
-              {JSON.stringify(JSON.parse(props.error.message), null, 2)}
-            </pre>
-          </div>
-        ) : null}
       </div>
-    );
+    </div>
+  );
+}
+
+function PreOptionsForm(props: {
+  integration: string;
+  options: IntegrationConnectInput[];
+  onSubmit: (options: Record<string, ConnectInputValue>) => void;
+  error: Error | null;
+}) {
+  const form = useForm<Record<string, ConnectInputValue>>();
+
+  return (
+    <div className="flex flex-col gap-4">
+      {props.options.map((option) => (
+        <Controller
+          key={option.id}
+          control={form.control}
+          name={option.id}
+          defaultValue={option.defaultValue}
+          render={({ field }) => (
+            <SerializedConnectInputPicker
+              key={option.id}
+              integration={props.integration}
+              field={option as SerializedConnectInput}
+              value={field.value}
+              onChange={field.onChange}
+            />
+          )}
+        />
+      ))}
+      <Button onClick={() => form.handleSubmit(props.onSubmit)()}>Next</Button>
+      <ErrorMessage error={props.error} />
+    </div>
+  );
+}
+
+function PostOptionsForm(props: {
+  integration: string;
+  options: IntegrationConnectInput[];
+  onSubmit: (options: Record<string, ConnectInputValue>) => void;
+  error: Error | null;
+}) {
+  const form = useForm<Record<string, ConnectInputValue>>();
+
+  return (
+    <div className="flex flex-col gap-4">
+      {props.options.map((option) => (
+        <Controller
+          key={option.id}
+          control={form.control}
+          name={option.id}
+          render={({ field }) => (
+            <SerializedConnectInputPicker
+              key={option.id}
+              integration={props.integration}
+              field={option as SerializedConnectInput}
+              value={field.value}
+              onChange={field.onChange}
+            />
+          )}
+        />
+      ))}
+      <Button onClick={() => form.handleSubmit(props.onSubmit)()}>
+        Finish
+      </Button>
+      <ErrorMessage error={props.error} />
+    </div>
+  );
+}
+
+function ErrorMessage(props: { error: Error | null }) {
+  if (!props.error) {
+    return null;
   }
 
-  if (props.installFlowStage.stage === 'postOptions') {
-    return (
-      <div className="flex flex-col gap-4">
-        {props.installFlowStage.options.map((option) => (
-          <SerializedConnectInputPicker
-            key={option.id}
-            integration={props.integration}
-            field={option as SerializedConnectInput}
-            value={postOptions[option.id]}
-            onChange={(value) => {
-              setPostOptions((current) => ({
-                ...current,
-                [option.id]: value,
-              }));
-            }}
-          />
-        ))}
-        <Button
-          onClick={() => {
-            props.onFinishPostOptions(postOptions);
-          }}
-        >
-          Finish
-        </Button>
-      </div>
-    );
-  }
-
-  return null;
+  return (
+    <div className="flex flex-col gap-2">
+      <p className="font-medium">Something went wrong</p>
+      <pre className="text-destructive max-w-full text-sm bg-destructive/10 p-2 rounded-md border border-destructive/20">
+        {JSON.stringify(JSON.parse(props.error.message), null, 2)}
+      </pre>
+    </div>
+  );
 }
